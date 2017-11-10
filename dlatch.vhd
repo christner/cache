1bddc43c0545b73674b71849630bd2fb715e365a
Daniel Christner <daniel.christner@gmail.com>
2017-11-10 13:25:59 -0500

    adding supplied components

---
+++ dlatch.vhd
@@ -0,0 +1,39 @@
+--------------------------------------------------------------------------------------------------
+--
+-- Entity: dff
+-- Architecture : structural
+-- Author: cpatel2
+-- Created On:
+-- Description: 
+--
+--------------------------------------------------------------------------------------------------
+
+library STD;
+library IEEE;                      
+
+use IEEE.std_logic_1164.all;       
+
+entity dlatch is port ( 
+    d   : in  std_logic;
+    clk : in  std_logic;
+    q   : out std_logic;
+    qbar: out std_logic); 
+end dlatch;                          
+
+architecture structural of dlatch is 
+begin
+  
+    output: process (d,clk)                  
+    begin                           
+        if clk = '1' 
+        then 
+
+            q <= d;
+            qbar <= not d ;
+
+        end if; 
+    end process output;        
+                             
+end structural;  
+
+--------------------------------------------------------------------------------------------------
