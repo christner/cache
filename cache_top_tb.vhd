055426c9bc8d8d08bc38c0fb5522d11ea5da4fb5
Daniel Christner <daniel.christner@gmail.com>
2017-11-17 13:44:36 -0500

    adding top module for state machine and cache integration and testbench

---
+++ cache_top_tb.vhd
@@ -0,0 +1,69 @@
+----------------------------------------------------------------------------------------------
+--
+-- Entity: cache_set_tb
+-- Architecture : test
+-- Author: danielc3
+-- Created On: 11/11/2017
+-- Description: Testbench for checking validity of tag, reading, and writing to
+--               blocks within set
+--
+----------------------------------------------------------------------------------------------
+
+library IEEE;
+
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_textio.all;
+
+use IEEE.numeric_std.all;
+
+use STD.textio.all;
+
+entity cache_top_tb is
+
+end cache_top_tb;
+
+architecture test of cache_top_tb is
+
+    component set_associative_cache_2 port (
+        enable         : in std_logic;
+        write_whole_blk: in std_logic;
+        w_r            : in std_logic;
+        address        : in std_logic_vector( 4 downto 0 );
+        data_w         : in std_logic_vector( 31 downto 0 );
+        hit_miss       : out std_logic;
+        data_r         : out std_logic_vector( 31 downto 0 ));
+    end component;
+
+    constant CLK_PERIOD : time := 10 ns;
+
+    for cache0 : set_associative_cache_2 use entity work.set_associative_cache_2(structural);
+
+    signal clock : std_logic;
+
+    signal enable, write_whole_blk, w_r : std_logic := '0';
+    signal address : std_logic_vector( 4 downto 0 ) := ( others => '0' );
+    signal data_w : std_logic_vector( 31 downto 0 ) := ( others => '0' );
+    signal hit_miss : std_logic := '0';
+    signal data_r : std_logic_vector( 31 downto 0 ) := ( others => '0' );
+
+begin
+
+    -- map inputs and ouputs
+    cache0 : set_associative_cache_2 port map (enable, write_whole_blk, w_r, address(4 downto 0), data_w(31 downto 0), hit_miss, data_r(31 downto 0));
+
+    clk : process
+    begin  -- process clk
+
+        clock <= '0','1' after CLK_PERIOD / 2;
+        wait for CLK_PERIOD;
+
+    end process clk;
+
+    io: process
+    begin -- process io
+
+        wait; -- done
+
+    end process io;
+
+end test;
---
+++ chip.vhd
@@ -0,0 +1,73 @@
+----------------------------------------------------------------------------------------------
+--
+-- Entity: cache_set
+-- Architecture : structural
+-- Author: danielc3
+-- Created On: 11/12/2017
+-- Description: complete set for cache consisting of 8 blocks
+--
+----------------------------------------------------------------------------------------------
+
+library STD;
+library IEEE;
+
+use IEEE.std_logic_1164.all;
+
+entity chip is port (
+    cpu_add    : in  std_logic_vector(7 downto 0);
+    cpu_data   : inout  std_logic_vector(7 downto 0);
+    cpu_rd_wrn : in  std_logic;
+    start      : in  std_logic;
+    clk        : in  std_logic;
+    reset      : in  std_logic;
+    mem_data   : in  std_logic_vector(7 downto 0);
+    Vdd	       : in  std_logic;
+    Gnd        : in  std_logic;
+    busy       : out std_logic;
+    mem_en     : out std_logic;
+    mem_add    : out std_logic_vector(7 downto 0));
+end chip;
+
+architecture structural of chip is
+
+    component state_machine port (
+        START : in std_logic;
+        RESET : in std_logic;
+        HitMiss : in std_logic;
+        RdWr : in std_logic;
+        CLOCK : in std_logic;
+        BUSY : out std_logic;
+        CacheEnable : out std_logic;
+        MemEnable : out std_logic);
+    end component;
+
+    component set_associative_cache_2 port (
+        enable         : in std_logic;
+        w_r            : in std_logic;
+        address        : in std_logic_vector( 7 downto 0 );
+        data_w         : in std_logic_vector( 7 downto 0 );
+        hit_miss       : out std_logic;
+        data_r         : out std_logic_vector( 7 downto 0 ));
+    end component;
+
+    for state_machine_0 : state_machine use entity work.state_machine(structural);
+
+    for cache_0 : set_associative_cache_2 use entity work.set_associative_cache_2(structural);
+
+    signal cache_enable, hit_miss : std_logic;
+    signal data : std_logic_vector( 7 downto 0 );
+
+begin
+
+    -- TODO: need to mux cpu data and mem_data into data
+
+    state_machine_0 : state_machine port map (start, reset, hit_miss, cpu_rd_wrn, clk, busy, cache_enable, mem_en);
+
+    cache_0 : set_associative_cache_2 port map (cache_enable, cpu_rd_wrn, cpu_add(7 downto 0),  data(7 downto 0), hit_miss, data_r(7 downto 0));
+
+    -- TODO: add 8 bit dlatches for mem_add -> cpu_add
+    -- TODO: add 8 bit dlatches for data_r -> cpu_data
+
+end structural;
+
+----------------------------------------------------------------------------------------------
