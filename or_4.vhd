fe4c0f8c0d264e3f9e9f802a7e1555cf02606c8a
Daniel Christner <daniel.christner@gmail.com>
2017-12-04 19:33:20 -0500

    (1) changed or_4.vhdl to port maps (2) updated git ignore (3) added vhdl work library, if you are having trouble getting modules to import, it's probably because you have the work library set to vhdl - and the modules that have already been imported exist in cachelib, not vhdl

--- or_4.vhd
+++ or_4.vhd
@@ -22,9 +22,22 @@ entity or_4 is port (
 end or_4;
 
 architecture structural of or_4 is
+
+  component or_2 port (
+      input1: in std_logic;
+      input2: in std_logic;
+      output: out std_logic);
+  end component;
+
+  for or_2_0, or_2_1, or_2_2 : or_2 use entity work.or_2(structural);
+
+  signal tmp_or_2_0, tmp_or_2_1 : std_logic;
+
 begin
 
-  output <= input4 or input3 or input2 or input1;
+  or_2_0 : or_2 port map(input1, input2, tmp_or_2_0);
+  or_2_1 : or_2 port map(input3, input4, tmp_or_2_1);
+  or_2_2 : or_2 port map(tmp_or_2_0, tmp_or_2_1, output);
 
 end structural;
 
