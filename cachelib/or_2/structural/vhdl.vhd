bb8eb29788fa0d69702d6b71b44c04ef11e6db72
Daniel Christner <daniel.christner@gmail.com>
2017-12-09 22:05:32 -0500

    or_2 finished & passing LVS

--- cachelib/or_2/structural/vhdl.vhd
+++ cachelib/or_2/structural/vhdl.vhd
@@ -1,5 +1,5 @@
 -- Created by @(#)$CDS: vhdlin version 6.1.7-64b 09/27/2016 19:46 (sjfhw304) $
--- on Sun Dec  3 17:08:40 2017
+-- on Sat Dec  9 20:28:39 2017
 
 
 architecture structural of or_2 is
